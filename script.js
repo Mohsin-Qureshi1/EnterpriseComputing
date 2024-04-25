0e29a5998b09a6c0f4e1d691ac0c3726080f49cd
Mohsin-Qureshi1 <muhammad.qureshi16@education.nsw.gov.au>
2024-04-25 14:52:05 +1000

    Version 5
    Connection to all pages through JS
    
    More CSS changes to homepage

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function HomeOpen() {   
+     window.location.replace('home.html')
+}
 
+function ContactOpen() {   
+    window.location.replace('Contact.html')
+}
+
+function TutorOpen() {   
+    window.location.replace('Tutor.html')
+}
+
+function MathsOpen() {   
+    window.location.replace('Maths.html')
+}
+
+function EnglishOpen() {   
+    window.location.replace('English.html')
+}
+
+function ScienceOpen() {   
+    window.location.replace('Science.html')
+}
