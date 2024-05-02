d3b3fbe819e7edbcb33572f91c94a590a6eeb55f
Mohsin-Qureshi1 <muhammad.qureshi16@education.nsw.gov.au>
2024-05-02 21:26:18 +1000

    Version 9.1
    Made functions for the buttons to navigate between year selected and content shown for subjects page. I hate JavaScript.

--- script.js
+++ script.js
@@ -21,3 +21,60 @@ function EnglishOpen() {
 function ScienceOpen() {   
     window.location.replace('Science.html')
 }
+
+
+function defaultSubjects() {
+    document.getElementById("MY7").style.backgroundColor = '#eee'
+    document.getElementById("MY8").style.backgroundColor = '#eee'
+    document.getElementById("MY9").style.backgroundColor = '#eee'
+    document.getElementById("MY10").style.backgroundColor = '#eee'
+    document.getElementById("MY11").style.backgroundColor = '#eee'
+
+    document.getElementById("yrM7content").style.display = "none"
+    document.getElementById("yrM7content").style.animationPlayState = "paused"
+    document.getElementById("yrM8content").style.display = "none"
+    document.getElementById("yrM8content").style.animationPlayState = "paused"
+    document.getElementById("yrM9content").style.display = "none"
+    document.getElementById("yrM9content").style.animationPlayState = "paused"
+    document.getElementById("yrM10content").style.display = "none"
+    document.getElementById("yrM10content").style.animationPlayState = "paused"
+    document.getElementById("yrM11content").style.display = "none"
+    document.getElementById("yrM11content").style.animationPlayState = "paused"
+}
+
+function MY7(){
+    defaultSubjects()
+    document.getElementById("yrM7content").style.display = "block"
+    document.getElementById("yrM7content").style.animationPlayState = "running"
+    document.getElementById("MY7").style.backgroundColor = '#fecd62'
+}
+
+function MY8(){
+    defaultSubjects()
+    document.getElementById("yrM8content").style.display = "block"
+    document.getElementById("yrM8content").style.animationPlayState = "running"
+    document.getElementById("MY8").style.backgroundColor = '#fecd62'
+}
+
+function MY9(){
+    defaultSubjects()
+    document.getElementById("yrM9content").style.display = "block"
+    document.getElementById("yrM9content").style.animationPlayState = "running"
+    document.getElementById("MY9").style.backgroundColor = '#fecd62'
+}
+
+function MY10(){
+    defaultSubjects()
+    document.getElementById("yrM10content").style.display = "block"
+    document.getElementById("yrM10content").style.animationPlayState = "running"
+    document.getElementById("MY10").style.backgroundColor = '#fecd62'
+}
+
+function MY11(){
+    defaultSubjects()
+    document.getElementById("yrM11content").style.display = "block"
+    document.getElementById("yrM11content").style.animationPlayState = "running"
+    document.getElementById("MY11").style.backgroundColor = '#fecd62'
+}
+
+
