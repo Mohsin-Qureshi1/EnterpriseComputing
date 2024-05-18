a3d7773423eea45206abf975b26a6af97e759a49
Mohsin-Qureshi1 <muhammad.qureshi16@education.nsw.gov.au>
2024-05-18 11:39:29 +1000

    Version 15 (Final)
    All webpages complete and mobile optimized.

--- script.js
+++ script.js
@@ -90,7 +90,135 @@ function MY12(){
 }
 
 function nextform() {
-    document.getElementById("parentsection").style.display = "block"
+    document.getElementById("parentsection").style.opacity = "1"
     document.getElementById("parentsection").style.animationPlayState = "running"
     document.getElementById("Nextbutton").style.display = "none"
+    document.getElementById("forma").style.borderBottomRightRadius = "0px";
+    document.getElementById("forma").style.borderBottomLeftRadius = "0px";
+
+}
+
+
+function checkallinputs() {
+    const subject = document.getElementById("Subjectinput");
+    const subjectinput = subject.value;
+
+    const studentname = document.getElementById("Studentname");
+    const studentnameinput = studentname.value;
+
+    const school = document.getElementById("schoolname");
+    const schoolinput = school.value;
+
+
+    if (subjectinput.length>=1) {
+        if (studentnameinput.length>=1) {
+            if (schoolinput.length>=1) {
+                document.getElementById("schoolname").style.border = "none";
+                nextform()
+            }
+            else {
+                document.getElementById("schoolname").style.border = "3px red solid";
+                document.getElementById("Nextbutton").textContent = "Retry";
+            }
+            document.getElementById("Studentname").style.border = "none";
+        }
+        else {
+            document.getElementById("Studentname").style.border = "3px red solid";
+            document.getElementById("Nextbutton").textContent = "Retry";
+        }
+        document.getElementById("Subjectinput").style.border = "none";
+        
+
+    }
+
+    else {
+        document.getElementById("Subjectinput").style.border = "3px red solid";
+        document.getElementById("Nextbutton").textContent = "Retry";
+        document.getElementById("Nextbutton").style.color = "red";
+    }
+
+}
+
+function submit() {
+    document.getElementById("schoolname").style.border = "3px solid #00028e";
+    document.getElementById("Subjectinput").style.border = "3px solid #00028e";
+    document.getElementById("Studentname").style.border = "3px solid #00028e";
+    document.getElementById("gradeselect").style.border = "3px solid #00028e";
+    document.getElementById("parentname").style.border = "3px solid #00028e";
+    document.getElementById("phone").style.border = "3px solid #00028e";
+    document.getElementById("email").style.border = "3px solid #00028e";
+    document.getElementById("submitbutton").style.border = "3px solid #00028e";
+    document.getElementById("submitbutton").style.color = "#00028e";
+    document.getElementById("submitbutton").textContent = "Submitted";
+    document.getElementById("submitbutton").style.width = "200px";
+}
+
+function submitform () {
+    const parent = document.getElementById("parentname");
+    const parentinput = parent.value;
+    const phone = document.getElementById("phone");
+    const phoneinput = phone.value;
+    const emailval = document.getElementById("email");
+    const email = emailval.value;
+
+
+    if (parentinput.length>=1) {
+        if (phoneinput.length>=1) {
+            if (ValidateEmail(email) == true) {
+
+                alert("Form Submitted");
+                submit();
+            }
+            else {
+                document.getElementById("email").style.border = "3px red solid";
+                document.getElementById("submitbutton").textContent = "Retry";
+                document.getElementById("submitbutton").style.color = "red";
+            }
+        }
+        else {
+            document.getElementById("phone").style.border = "3px red solid";
+            document.getElementById("submitbutton").textContent = "Retry";
+            document.getElementById("submitbutton").style.color = "red";
+        }
+    }
+    else {
+        document.getElementById("parentname").style.border = "3px red solid";
+        document.getElementById("submitbutton").textContent = "Retry";
+        document.getElementById("submitbutton").style.color = "red";
+    }
+
+    
+
+}
+
+function ValidateEmail(input) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; 
+
+    if (input.match(validRegex)) { 
+         return true;
+    } 
+    else { 
+        return false;
+    }
+}
+
+function newsletter(){
+    const email = document.getElementById("email");
+    const emailinput = email.value;
+
+    if (ValidateEmail(emailinput) == true) {
+        alert("Email Subscribed");
+        document.getElementById("email").style.border = "3px #00028e solid";
+        document.getElementById("newslettersubmit").style.border = "3px #00028e solid";
+        document.getElementById("newslettersubmit").style.color = "#00028e";
+        document.getElementById("newslettersubmit").style.width = "120px";
+        document.getElementById("newslettersubmit").textContent = "Submitted";
+    }
+    else {
+        document.getElementById("email").style.border = "3px red solid";
+        document.getElementById("newslettersubmit").style.border = "3px red solid";
+        document.getElementById("newslettersubmit").style.color = "red";
+        document.getElementById("newslettersubmit").textContent = "Retry";
+    }
 }
